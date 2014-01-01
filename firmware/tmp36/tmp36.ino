55d3f8e1d7f494a7af0dbe88c9e54f5d9e34deef
baldnate <baldnate@gmail.com>
2013-12-31 21:47:20 -0600

    Comments fixed, dead code removed.

--- firmware/tmp36/tmp36.ino
+++ firmware/tmp36/tmp36.ino
@@ -1,14 +1,12 @@
 /*
-  ear.ino - firmware for wireless listener and temp node
+  tmp36.ino - firmware for wireless listener and temp node
 
   by Bald Nate
 
-  tmp102 code based on info from http://bildr.org/2011/01/tmp102-arduino/
-
   Hardware needed:
   434MHz RF Receiver - https://www.sparkfun.com/products/10532
-  TMP102 - https://www.sparkfun.com/products/11931
-  An Arduino (I used an Uno)
+  TMP36 - https://www.sparkfun.com/products/10988
+  An Arduino (I used an old Diecimila)
 
   Libraries needed:
   VirtualWire - http://www.airspayce.com/mikem/arduino/VirtualWire/
@@ -20,10 +18,7 @@
   * Both the RF protocol and the serial protocol need defining.
 */
 
-
 #include <VirtualWire.h>
-
-const int tmp102Address = 0x48; // A0 pin tied to ground
 
 float getTempF()
 {
@@ -34,22 +29,15 @@ float getTempF()
 
 void setup()
 {
-//  Serial.begin(9600);
-  
   pinMode(0, INPUT);
-
   vw_set_ptt_inverted(true); // Required for DR3100
   vw_setup(2000);	     // Bits per sec
   vw_set_tx_pin(4);
-
-//  Serial.println("tmp36 online");
 }
 
 void loop()
 {
   float fahrenheit = getTempF();
-//  Serial.print("tmp36F:");
-//  Serial.println(fahrenheit);
 
   // TODO: Create RF message format, do that instead of both the serial prints and the RF test data.
   char *msg = "tmp36-09876543210987654321\n";
