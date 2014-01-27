7c6ffac8ea495cea4d9948059e7dd2224b5306e4
baldnate <baldnate@gmail.com>
2014-01-26 21:05:12 -0600

    No longer needed.

--- host/send_tweet.py
+++
@@ -1,8 +0,0 @@
-from twython import Twython
-import json
-
-secrets = json.load(open('secrets.json'))
-
-twitter = Twython(secrets['APP_KEY'], secrets['APP_SECRET'], secrets['OAUTH_TOKEN'], secrets['OAUTH_TOKEN_SECRET'])
-
-twitter.update_status(status="TESTING TESTING.")
