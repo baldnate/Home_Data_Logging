0c708bff0ba85bc1e6a90d27cebd43cd1f4d6152
baldnate <baldnate@gmail.com>
2014-01-27 21:47:10 -0600

    Added support for "RapidFire" updates.

--- host/wx_pws.py
+++ host/wx_pws.py
@@ -11,25 +11,34 @@ class WundergroundPWS(object):
 
     """docstring for WundergroundPWS"""
 
-    def __init__(self, account, password):
+    def __init__(self, account, password, rtfreq=False):
         super(WundergroundPWS, self).__init__()
-        self.stock = {
-            'action': 'updateraw',
-            'softwaretype': 'baldwx'
-        }
         self.secrets = {
             'ID': account,
             'PASSWORD': password
         }
+        self.stock = {
+            'action': 'updateraw',
+            'softwaretype': 'baldwx'
+        }
+        self.realtime = bool(rtfreq)
+        if self.realtime:
+            self.stock['realtime'] = 1
+            self.stock['rtfreq'] = rtfreq
 
     def update(self, **kwargs):
         obs = dict((k, v) for k, v in kwargs.iteritems() if v is not None)
-        args = dict(obs.items() + stock.items() + secrets.items())
+        args = dict(obs.items() + self.stock.items() + self.secrets.items())
         params = urllib.urlencode(args)
+        bld = ""
+        if self.realtime:
+            bld = 'rtupdate'
+        else:
+            bld = 'weatherstation'
         f = urllib.urlopen(
-            "http://weatherstation.wunderground.com/weatherstation/updateweatherstation.php?%s" % params)
+            "http://%s.wunderground.com/weatherstation/updateweatherstation.php?%s" % (bld, params))
         result = f.read()
-        if result == 'success':
+        if 'success' in result:
             return
         else:
             raise Exception(result)
