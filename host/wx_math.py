f0637e680a0b6fa2e10c16741c1f4c1b323c425a
baldnate <baldnate@gmail.com>
2014-01-27 20:16:14 -0600

    Removed dead code.

--- host/wx_math.py
+++ host/wx_math.py
@@ -152,16 +152,6 @@ def degreesToCompass(d):
     return directions[int(d / 45)]
 
 
-def degreesToArrow(d):
-    """
-    Takes degrees, returns unicode arrow for that compass direction.
-    """
-    directions = "↑ ↗ → ↘ ↓ ↙ ← ↖".split()
-    directions *= 2  # no need for modulo later
-    d = (d % 360) + 360 / 16
-    return directions[int(d / 45)]
-
-
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
